96d7b19026c2afc8dd18ea60cf822add1f742f12
Shawn Talvacchia <stalvacchia@gmail.com>
2019-09-10 11:28:49 +0700

    up1

---
+++ app.py
@@ -0,0 +1,314 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_login import LoginManager, UserMixin, login_user, logout_user, current_user, login_required
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import desc
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField, TextAreaField
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError, Length
+from flask_migrate import Migrate
+
+
+# define app
+app = Flask(__name__)
+app.secret_key = "secret sauce"
+login_manager = LoginManager(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+POSTGRES = {
+    'user': "shawn",
+    'pw': "123",
+    'db': "blog",
+    'host': "localhost",
+    'port': 5432,
+}
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:%(pw)s@%(host)s:\
+%(port)s/%(db)s' % POSTGRES
+
+
+#######################################################################
+
+# define models
+
+class Flags(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+
+class Users(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False, unique=True)
+    email = db.Column(db.String(128), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    posts = db.relationship('Posts', backref="users", lazy="dynamic")
+    comments = db.relationship('Comments', backref="users", lazy="dynamic")
+    flags = db.relationship('Flags', backref=db.backref(
+        'users', lazy=True))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+class Posts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    author_id = db.Column(
+        db.Integer, db.ForeignKey('users.id'), nullable=False)
+    title = db.Column(db.Text, nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    created = db.Column(db.DateTime, nullable=False)
+    updated = db.Column(db.DateTime)
+    comments = db.relationship('Comments', backref="posts", lazy="dynamic")
+    view_count = db.Column(db.Integer, default=0)
+    flags = db.relationship('Flags', backref=db.backref(
+        'posts'), lazy=True)
+
+
+class Comments(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.Text, nullable=False)
+    created = db.Column(db.DateTime, nullable=False)
+    updated = db.Column(db.DateTime)
+    author_id = db.Column(
+        db.Integer, db.ForeignKey('users.id'), nullable=False)
+    post = db.Column(db.Integer, db.ForeignKey('posts.id'), nullable=False)
+
+
+db.create_all()
+
+
+#######################################################################
+
+# define forms
+
+class Register(FlaskForm):
+    username = StringField("USERNAME", validators=[DataRequired()])
+    email = StringField("EMAIL", validators=[
+                        DataRequired(), Email("Input a valid email"),
+                        Length(min=3, max=20, message="email must be between 3 and 20 characters in length")])
+    password = PasswordField("PASSWORD", validators=[
+                             DataRequired(), EqualTo("confirm")])
+    confirm = PasswordField("CONFIRM PASSWORD", validators=[DataRequired()])
+    submit = SubmitField("REGISTER")
+
+    def validate_username(self, field):
+        if Users.query.filter_by(username=field.data).first():
+            raise ValidationError("Your username has already been registered!")
+
+    def validate_email(self, field):
+        if Users.query.filter_by(email=field.data).first():
+            raise ValidationError("Your email has already been registered!")
+
+
+class Loginform(FlaskForm):
+    email = StringField("EMAIL", validators=[
+                        DataRequired(), Email("Input a valid email"),
+                        Length(min=3, max=20, message="email must be between 3 and 20 characters in length")])
+    password = PasswordField("PASSWORD", validators=[
+                             DataRequired()])
+    submit = SubmitField("Login")
+
+
+class New_post(FlaskForm):
+    title = StringField("Title", validators=[
+                        DataRequired(),
+                        Length(min=3, max=30, message="What's on your mind?")])
+    body = TextAreaField("Body", render_kw={"rows": 50, "cols": 10}, validators=[
+        DataRequired(),
+        Length(min=3, max=10000, message="You got 3000 characters to let us know how you feel!")])
+    submit = SubmitField("Post")
+
+
+class New_comment(FlaskForm):
+    body = StringField(render_kw={"placeholder": "Share your thoughts.."}, validators=[
+                       DataRequired(), Length(max=100, message="Make it snappy!")])
+    submit = SubmitField("Comment")
+#######################################################################
+
+
+@login_manager.user_loader
+def load_user(id):
+    return Users.query.get(int(id))
+
+
+@app.route('/')
+def home():
+    template = ['index.html', 'includes/something.html']
+    posts = Posts.query.order_by(desc(Posts.id))
+    return render_template(template, posts=posts)
+
+
+@app.route('/profile/<id>')
+@login_required
+def profile():
+    posts = Posts.query.filter_by(
+        author_id=current_user.id).order_by(desc(Posts.id))
+    return render_template('profile.html', posts=posts, id=current_user.id)
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    form = Loginform()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            user = Users.query.filter_by(email=form.email.data).first()
+            if user and user.check_password(form.password.data):
+                login_user(user)
+                flash('Welcome!', 'success')
+                return redirect(url_for('home'))
+            else:
+                flash('Incorrect username/password', 'danger')
+                return redirect(url_for('login'))
+        else:
+            flash('Try Again! You input something wrong!', 'danger')
+            return redirect(url_for('login'))
+    return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    form = Register()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            new_user = Users(email=form.email.data,
+                             username=form.username.data)
+            new_user.set_password(form.password.data)
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user)
+            flash('Welcome!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Incorrect username/password', 'danger')
+            return redirect(url_for('register'))
+    return render_template("register.html", form=form)
+
+
+@app.route('/newpost', methods=['POST', 'GET'])
+@login_required
+def newpost():
+    form = New_post()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            new_post = Posts(title=form.title.data,
+                             body=form.body.data,
+                             created=datetime.now())
+            current_user.posts.append(new_post)
+            db.session.add(new_post)
+            db.session.commit()
+            return redirect(url_for('home'))
+        else:
+            for field_name, errors in form.errors.items():
+                flash(errors)
+            return redirect(url_for('newpost'))
+    return render_template('new_post.html', form=form)
+
+
+@app.route('/editpost/<id>', methods=['POST', 'GET'])
+@login_required
+def editpost(id):
+    post = Posts.query.filter_by(id=id, author_id=current_user.id).first()
+    if post:
+        form = New_post()
+        if request.method == 'POST':
+            if form.validate_on_submit():
+                post.title = form.title.data
+                post.body = form.body.data
+                post.updated = datetime.now()
+                db.session.commit()
+                return redirect(url_for('home'))
+            else:
+                for field_name, errors in form.errors.items():
+                    flash(errors)
+                return redirect(url_for('editpost'), id=id)
+    else:
+        flash("Go edit your own posts!", 'danger')
+        return redirect(url_for('home'))
+    return render_template('editpost.html', form=form, post=post)
+
+
+@app.route('/deletepost/<id>', methods=['POST', 'GET'])
+@login_required
+def deletepost(id):
+    post = Posts.query.filter_by(id=id, author_id=current_user.id).first()
+    if post:
+        db.session.delete(post)
+        db.session.commit()
+    else:
+        flash(['You are not allowed to delete this post'])
+    return redirect(url_for('home'))
+
+
+@app.route('/single_post/<id>', methods=['POST', 'GET'])
+def single_post(id):
+    template = ['single_post.html', 'includes/new_comment.html']
+    post = Posts.query.filter_by(id=id).first()
+    comments = post.comments.all()
+    post.view_count += 1
+    check = Flags.query.filter_by(user_id=current_user.id, post_id=id).first()
+    if check:
+        is_flag = True
+    else:
+        is_flag = False
+    
+    
+    form = New_comment()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            post = Posts.query.filter_by(id=id).first()
+            c = Comments(body=form.body.data,
+                        created=datetime.now(),
+                        )
+            current_user.comments.append(c)  # author
+            post.comments.append(c)      # post
+            db.session.add(c)
+            db.session.commit()
+            return redirect(url_for('single_post', id=id))
+        else:
+            for field_name, errors in form.errors.items():
+                flash(errors)
+            return redirect((url_for('new_comment')))
+
+
+    db.session.commit()
+    return render_template(template, post=post, comments=comments, is_flag=is_flag, form=form)
+
+
+@app.route('/single_post/<id>/flag', methods=['POST', 'GET'])
+@login_required
+def flag_post(id):
+    post = Posts.query.filter_by(id=id).first()
+    check = Flags.query.filter_by(user_id=current_user.id, post_id=id).first()
+    if not check:
+        flag = Flags(user_id = current_user.id,
+                                post_id=id)
+        db.session.add(flag)
+    else:
+        db.session.delete(check)
+    db.session.commit()
+    return redirect(url_for('single_post', id=id))
+
+
+
+
+
+@app.route('/top_posts')
+def top_posts():
+    template = ['top_post.html', 'includes/something.html']
+    posts = Posts.query.order_by(desc(Posts.view_count))
+    return render_template('top_posts.html', posts=posts)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
